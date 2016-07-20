58af2c96206b36aee835cd2879c2d7f410eea39f
Matthew <matthew.whitehead@coloradocollege.edu>
2016-07-19 21:00:30 -0600

    Working on Graph class

--- npann.py
+++ npann.py
@@ -222,6 +222,7 @@ class FiniteDifference(Layer):
 
 
 
+
 class AiboPG(Layer):
     
     def __init__(self, number_incoming, number_outgoing, activation='sigmoid', weight_init='glorot_uniform'):
@@ -444,6 +445,18 @@ class Dense(Layer):
         self.weights += self.prev_update
 
 
+
+class Merge(Layer):
+    
+    def forward(self, list_inputs):
+        return np.stack(list_inputs)
+    
+    def backward(self, grad):
+        return grad
+
+
+
+
 class Sequential:
     
     def __init__(self):
@@ -538,19 +551,21 @@ class Graph:
         self.layers = {}  # each layer has a name
         self.backward_connections = {} # links from key back to value (both layer names)
         self.forward_connections = {} # links from key forward to value (both layer names)
-        self.output_layers = {} # names of layers that output from network
+        self.output_layers = [] # names of layers that output from network
         self.input_layers = {} # names of inputs
         
     def addLayer(self, layer_name, layer, inputs, is_output=False):
         self.layers[layer_name] = layer
         self.backward_connections[layer_name] = []
+        if is_output:
+            self.output_layers.append(layer_name)
                 
-        for inp in inputs:
-            prev_layer = self.layers[inp]
-            if prev_layer not in self.forward_connections:
-                self.forward_connections[prev_layer] = []
-            self.forward_connections[prev_layer].append(layer_name)
-            self.backward_connections[layer_name].append(prev_layer)
+        for incoming_layer in inputs:
+            #prev_layer = self.layers[inp]
+            if incoming_layer not in self.forward_connections:
+                self.forward_connections[incoming_layer] = []
+            self.forward_connections[incoming_layer].append(layer_name)
+            self.backward_connections[layer_name].append(incoming_layer)
 
         
     def addLoss(self, loss_layer):
@@ -562,23 +577,72 @@ class Graph:
     def setInput(self, input_name, X):
         self.input_layers[input_name] = X
         
-    def forward(self, named_input_pairs):  # pairs of (name, X) for inputs
-        
-        for input_name, X in named_input_pairs:
-            self.setInput(input_name, X)
-            
-            
-        # KEEP WORKING HERE, YOU GOOFBALL    
-            
-        
-        curr_x = X
-        for i in range(len(self.layers)):
-            curr_x = self.layers[i].forward(curr_x)
-        return curr_x
+    def forwardComputationComplete(self, layers_computed):
+        for layer_name in layers_computed:
+            if layers_computed[layer_name] == False:
+                return False
+        return True
     
-    def backward(self, output, target):
-        self.loss = self.loss_layer.calculateLoss(output, target)
-        curr_grad = self.loss_layer.calculateGrad(output, target)
+    def forwardLayerInputsReady(self, layer_name, layers_computed):
+        for incoming_layer in self.backward_connections[layer_name]:
+            if not layers_computed[incoming_layer]:
+                return False
+        return True
+
+        
+    def forward(self, named_input_pairs):  # pairs of (name, X) for inputs
+        # Set up the inputs to the entire Graph
+        for input_name in named_input_pairs:
+            self.setInput(input_name, named_input_pairs[input_name])
+            
+            
+        layer_outputs = {}
+        
+        # Init all layers to need to be computed
+        layers_computed = {}
+        for layer_name in self.layers:
+            layers_computed[layer_name] = False
+        for layer_name in self.input_layers:
+            layers_computed[layer_name] = True
+            layer_outputs[layer_name] = self.input_layers[layer_name]
+            
+        print "layers computed", layers_computed
+        
+
+            
+        while not self.forwardComputationComplete(layers_computed):
+            for layer_name in self.layers:
+                if not layers_computed[layer_name]:
+                    if self.forwardLayerInputsReady(layer_name, layers_computed):
+                        if self.layers[layer_name].__class__.__name__ != "Merge":
+                            # Only has one input!
+                            print "COMPUTING:", layer_name
+                            incoming_layer = self.backward_connections[layer_name][0]
+                            layer_outputs[layer_name] = self.layers[layer_name].forward(layer_outputs[incoming_layer]) 
+                            layers_computed[layer_name] = True
+                        else:
+                            # Treat a merging layer differently
+                            merge_inputs = []
+                            for incoming_layer in self.backward_connections[layer_name]:
+                                merge_inputs.append(layers_computed[incoming_layer])
+                            self.layer_outputs[layer_name] = self.layers[layer_name].forward(merge_inputs)  
+                            layers_computed[layer_name] = True
+            
+        # Should output multiple values in the future?
+        # for now only output a single value...
+        for layer_name in self.output_layers:
+            print "OUTPUT:", layer_name
+            return layer_outputs[layer_name]
+
+        return "ERROR!!!"
+    
+    
+    
+    def backward(self, curr_grad):
+        # Again, assume a single output layer for now...
+        curr_layer = self.output_layers[0]
+
+        # WORKING HERE...:P
 
         for i in range(len(self.layers)-1, -1, -1):
             curr_grad = self.layers[i].backward(curr_grad)
@@ -591,7 +655,10 @@ class Graph:
 
     def iterate(self, X, y):
         output = self.forward(X)
-        final_grad = self.backward(output, y)
+        print "OUTPUT:", output
+        self.loss = self.loss_layer.calculateLoss(output, y)
+        curr_grad = self.loss_layer.calculateGrad(output, y)
+        final_grad = self.backward(curr_grad)
         self.update()
         return np.linalg.norm(self.loss)
     
@@ -600,7 +667,7 @@ class Graph:
             layer.reset()
     
     def train(self, X, y, minibatch_size, number_epochs, verbose=True):
-        self.resetLayers()  # clear out any old tables/state
+        #self.resetLayers()  # clear out any old tables/state
         dataset_size = len(X)
         for i in range(number_epochs):
             all_minibatch_indexes = np.random.permutation(dataset_size)
@@ -609,7 +676,8 @@ class Graph:
                 minibatch_indexes = all_minibatch_indexes[j * minibatch_size:j * minibatch_size + minibatch_size]
                 minibatch_X = X[minibatch_indexes]
                 minibatch_y = y[minibatch_indexes]
-                minibatch_err = self.iterate(minibatch_X, minibatch_y)
+                inputs = {"input1": minibatch_X}
+                minibatch_err = self.iterate(inputs, minibatch_y)
                 epoch_err += minibatch_err
                 
             if verbose:
---
+++ npann_graph_test.py
@@ -0,0 +1,27 @@
+import numpy as np
+import h5py
+import random
+from npann_functions import *
+from npann import *
+
+
+if __name__ == "__main__":
+    ann = Graph()
+    ann.addLayer("dense1", Dense(9, 30), ["input1"])
+    ann.addLayer("dense2", Dense(30, 1), ["dense1"], is_output=True)
+    ann.addLoss(MSE())
+
+    X, y = loadBreastCancer() #loadXOR()
+    minibatch_size = 4
+    epochs_per_chunk = 1000
+    number_epochs = 50000
+
+    for epoch in range(number_epochs / epochs_per_chunk):
+        ann.train(X, y, minibatch_size, epochs_per_chunk, verbose=False)
+        output = ann.forward(X)
+
+        corr = 0
+        for i in range(len(output)):
+            if (output[i] < 0.5 and y[i] == 0) or (output[i] >= 0.5 and y[i] == 1):
+                corr += 1
+        print "ACCURACY:", corr / float(len(output))
